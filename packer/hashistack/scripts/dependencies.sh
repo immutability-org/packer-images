85ed85b3e3b962708e68c5cec80a4dcbad2b00f4
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-12-16 08:45:57 -0500

    Python 2.7.12

--- packer/hashistack/scripts/dependencies.sh
+++ packer/hashistack/scripts/dependencies.sh
@@ -14,4 +14,34 @@ apt-get -y install curl unzip wget git jq curl nginx nginx-extras apache2-utils 
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
+apt-get -y install build-essential checkinstall ksh git unzip awscli
+#for some reason this just won't be quiet. Maybe a vagrant thing.
+apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+cd /usr/src
+wget --progress=bar:force https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
+
+echo -e "\n[python] untar...\n";
+
+tar xzf Python-2.7.12.tgz
+cd Python-2.7.12
+
+echo -e "\n[python] configure...\n";
+
+./configure > /tmp/python-configure.log 2>&1
+
+echo -e "\n[python] compiling...\n";
+
+make install > /tmp/python-make-install.log 2>&1
+wget --progress=bar:force https://bootstrap.pypa.io/get-pip.py
+
+echo -e "\n[python] get-pip bootstrap...\n";
+
+python get-pip.py > /tmp/get-pip.log 2>&1
+
+echo -e "\n[python] pip and other stuff...\n";
+
+pip install --quiet --upgrade pip
+pip install --quiet --upgrade virtualenv
+
+
 logger "Completed"
